be6ea3d8b7fccf5058207063dd7baa050105ade2
Faysal Akbar <faysalrj361@gmail.com>
2021-10-23 11:27:05 +0600

    react mongo crud

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+
+    const handleAddUser = (e) => {
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        // console.log(name, email);
+        const newUser = {name, email};
+
+        fetch('http://localhost:5000/users', {
+            method:"post",
+            headers: {
+                "content-type":"application/json",
+            },
+            body: JSON.stringify(newUser),
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                alert('successfully added the user');
+                e.target.reset();
+            }
+        })
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h2>This is Add User</h2>
+            <h2>Please add an user</h2>
+            <form onSubmit={handleAddUser}>
+                <input ref={nameRef} type="text" placeholder="Name" /><br />
+                <input ref={emailRef} type="email" name="email" id="" placeholder="Email" /><br />
+                <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const UpdateUser = () => {
+    const {id} = useParams();
+    const [user, setUser] = useState({});
+
+    useEffect(()=> {
+        fetch(`http://localhost:5000/users/${id}`)
+        .then(res => res.json())
+        .then(data => setUser(data))
+    },[])
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2> Update {user.name}</h2>
+            <p>{id}</p>
         </div>
     );
 };
